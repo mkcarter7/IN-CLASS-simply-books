a96d8312d4eb99ddbd955d06114d6688927ecf69
Melissa Carter <mkd.princess@gmail.com>
2024-07-08 15:12:30 -0500

    added data routes

---
+++ pages/author/edit/[firebaseKey].js
@@ -0,0 +1,6 @@
+/* eslint-disable react-hooks/rules-of-hooks */
+import { useRouter } from 'next/router';
+
+const router = useRouter();
+// eslint-disable-next-line no-unused-vars
+const { firebaseKey } = router.query;
